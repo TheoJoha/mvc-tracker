9719b1fb8e6b81fd9167949cce1561ab59f4f36e
Theodor Johansson <t.j.u@telia.com>
2023-12-13 12:28:50 +0100

    controller functionality in index

--- view/course_list.php
+++ view/course_list.php
@@ -4,9 +4,42 @@
         <section id="list" class="list">
             <header class="list__row list__header">
                 <h1>Coruse List</h1>
-    </header>   
+    </header>
+
+    <?php foreach ($courses as $course) : ?>
+        <div class="list__row">
+            <div class="list_item">
+                <p class="bold"><?= $course['courseName'] ?></p>
+            </div>
+            <div class="list__removeItem">
+                <form action="." method="post">
+                    <input type="hidden" name="action" value="delete_course">
+                    <input type="hidden" name="course_id" value="<?= $course['courseID'] ?>">
+                    <button class="remove-button">Remove</button>
+    </form>
+    </div>
+    </div>
+    <?php endforeach ?>
+    </section>
     <?php } else { ?>
             <p>No courses exist yet.</p>
     <?php } ?>
+
+    <section id="add" class="add">
+        <h2>Add Course</h2>
+        <form action="." method="post" id="add__form" class="add__form">
+            <input type="hidden" name="action" value="add_course">
+            <div class="add__input">
+                <label>Name:</label>
+                <input type="text" name="course_name" maxlength="50"
+                placeholder="Name" autofocus required>
+    </div>
+    <div class="add__addItem">
+        <button class="add-button bold">Add</button>
+    </div>
+    </form>
+    </section>
+    <br>
+    <p><a href=".">View &amp; Add Assignments</a></p>
 
 <?php include('view/footer.php') ?>
