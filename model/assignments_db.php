6e269e286a3778f3dda97d80aedfbabe3514ce8a
Theodor Johansson <t.j.u@telia.com>
2023-12-13 08:07:40 +0100

    views, functions

---
+++ model/assignments_db.php
@@ -0,0 +1,27 @@
+<?php
+
+    function get_assignments_by_course($course_id) {
+        global $db;
+        if ($course_id) {
+            $query = 'SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN
+            courses C ON A.courseID = C.courseID WHERE A.courseID = :course_id ORDER BY A.ID';
+        } else {
+            $query = 'SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN
+            courses C ON A.courseID = C.courseID  ORDER BY C.courseID';
+        }
+        $statement = $db->prepare($query);
+        $statement->bindValue(':course_id', $course_id);
+        $statement->execute();
+        $assignments = $statement->fetchAll();
+        $statement->closeCursor();
+        return $assignments;
+    }
+
+    function delete_assignment( $assignment_id ) {
+        global $db;
+        $query = 'DELETE FROM assignments WHERE ID = :assign_id';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':course_id', $assignment_id);
+        $statement->execute();
+        $statement->closeCursor();
+    }
--- view/header.php
+++ view/header.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Assignment Tracker</title>
+    <link rel="stylesheet" href="view/css/main.min.css" />
 </head>
 <body>
-    
-</body>
-</html>
+    <main class="main">
