b15cfde9ea18489eed07ba3dc0ecdbf64485c812
Theodor Johansson <t.j.u@telia.com>
2023-12-13 07:54:23 +0100

    first commit

---
+++ model/database.php
@@ -0,0 +1,14 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=assignment_tracker';
+    $username = 'root';
+    //$password = 'pa55word';
+
+    try {
+        $db = new PDO($dsn, $username);
+    } catch (PDOException $e) {
+        $error = "Database Error: ";
+        $error .= $e->getMessage();
+        include('view/error.php');
+        exit();
+    }
+?>
---
+++ view/error.php
@@ -0,0 +1,6 @@
+<?php include('header.php') ?>
+<h2>Error</h2>
+<p><?= $error ?></p>
+<br>
+<p><a ref=".">Back to List</a></p>
+<?php include('footer.php') ?>
